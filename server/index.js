af8bcae80c2e6059be5545329ff4d3a405e1b71b
Abhishek Anand <120253524+anand-1Abhishek@users.noreply.github.com>
2024-09-15 00:28:07 +0530

    Done basic setup

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const PORT =  5000;
+
+app.get('/',(req, res)=>{
+    res.send('welcome to insta')
+})
+
+app.listen(PORT,()=>{
+    console.log(`app is running on port ${PORT}`)
+})
